1dc0ffce316f976c233bdc68a3e2d1ce1c20a5e3
Allen Davis <allendaviskoonan@gmail.com>
2020-10-26 13:49:37 +0530

    Added InventoryDisplay scene

--- Inventory.gd
+++ Inventory.gd
@@ -10,14 +10,14 @@ export (Array, Resource) var items: Array = [
 ]
 
 
-func set_item(item_index: int, item: Item):
+func set_item(item_index: int, item: Item) -> Item:
 	var previous_item = items[item_index] #stores the previous_item
 	items[item_index] = item #sets the item_index index in items to the passed in tem
 	emit_signal("items_changed",[item_index]) 
 	return previous_item
 
 
-func swap_items(item_index: int, target_item_index: int):
+func swap_items(item_index: int, target_item_index: int) -> void:
 	#stores both the target item and the item to be swapped
 	var target_item = items[target_item_index] 
 	var item = items[item_index]
@@ -27,7 +27,7 @@ func swap_items(item_index: int, target_item_index: int):
 	emit_signal("item_changed",[item_index, target_item_index])
 
 
-func remove_item(item_index: int):
+func remove_item(item_index: int) -> Item:
 	var previous_item = items[item_index]
 	items[item_index] = null #sets the item_index index in items to null
 	emit_signal("items_changed",[item_index])
