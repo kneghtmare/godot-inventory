70c40fd15146a96954bec2286ed75c98fd82defd
Allen Davis <allendaviskoonan@gmail.com>
2020-10-26 15:27:46 +0530

    Deleted the two functions in InventoryDisplay and replaced them with one single function that updates all slot displays. This is done by looping over children and updating the display of each of them.

--- InventoryContainer.gd
+++ InventoryContainer.gd
@@ -6,5 +6,6 @@ var inventory: Inventory = preload("res://Inventory.tres")
 func can_drop_data(_position: Vector2, data):
 	return data is Dictionary and data.has("item")
 
+
 func drop_data(_position: Vector2, data):
 	inventory.set_item(data.item_index, data.item)
--- InventoryDisplay.gd
+++ InventoryDisplay.gd
@@ -4,20 +4,13 @@ var inventory: Inventory = preload("res://Inventory.tres")
 
 func _ready() -> void:
 	inventory.connect("items_changed",self,"_on_items_changed")
-	update_inventory_display()
+	update_slot_displays()
 
 
-func update_inventory_display() -> void:
-	for item_index in inventory.items.size():
-		update_inventory_slot_display(item_index)
-
-
-func update_inventory_slot_display(item_index: int) -> void:
-	var inventorySlotDisplay: InventorySlotDisplay = get_child(item_index)
-	var item: Item = inventory.items[item_index]
-	inventorySlotDisplay.display_item(item)
-
+func update_slot_displays() -> void:
+	for child in get_children():
+		child.update_display()
+	
 
 func _on_items_changed(indexes: Array) -> void:
-	for item_index in indexes:
-		update_inventory_slot_display(item_index)
+	update_slot_displays()
