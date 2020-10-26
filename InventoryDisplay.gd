4b1f5b069326f9a5abe6121e74bec5f6faa97834
Allen Davis <allendaviskoonan@gmail.com>
2020-10-26 14:20:12 +0530

    Displays the correct items now

--- InventoryDisplay.gd
+++ InventoryDisplay.gd
@@ -1,16 +1,22 @@
 extends GridContainer
 
-var inventory = preload("res://Inventory.tres")
+var inventory: Inventory = preload("res://Inventory.tres")
 
 func _ready() -> void:
 	inventory.connect("items_changed",self,"_on_items_changed")
+	update_inventory_display()
 
 
-func update_inventory_slot_display(item_index):
+func update_inventory_display() -> void:
+	for item_index in inventory.items.size():
+		update_inventory_slot_display(item_index)
+
+
+func update_inventory_slot_display(item_index: int) -> void:
 	var inventorySlotDisplay: InventorySlotDisplay = get_child(item_index)
 	var item = inventory.items[item_index]
 	inventorySlotDisplay.display_item(item)
-	
+
 
 func _on_items_changed(indexes: Array) -> void:
 	for item_index in indexes:
